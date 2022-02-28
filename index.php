da8e70f95f245e8a2e1fbf69f74a747df17ddd74
ikrzmcxx <99166161+ikrzmcxx@users.noreply.github.com>
2022-02-28 16:25:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head> <title> Performance task in Mathematics 9 </head> </title>
+<body style="background-color:red;">
+
+<h1 style="font-size:300%;"> Performance Task In Mathematics</h1>
+      
+ <p> <h1 style="font- size:150%;"> <p style="font-family:courier;"> <p>“Everything around you is numbers, everything around you is mathematics”. </h1> </p>
+
+   <p> Clicke here! </p>
+<a href="https://drive.google.com/drive/folders/1FKP15FNAlbe4UQ96Rx8WCfC0yiIorJm1?usp=sharing"> click here </a>
+</body>
+</html> 
